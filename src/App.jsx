79c0f986e74f8d0a4d9d03d0aa3572c63ddc439b
Kresna Satya <kresnasatya72@gmail.com>
2024-10-29 08:06:21 +0800

    implement tabs in textarea, fix tree element by set width, and implement copy to clipboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,54 @@ import { useState } from "react"
 
 function App() {
   const [value, setValue] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  console.log(value);
+
+  function handleTabKey(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+
+      const { selectionStart, selectionEnd } = event.target;
+      const value = event.target.value;
+      
+      // Insert tab at cursor position
+      event.target.value = value.substring(0, selectionStart) + `\t` + value.substring(selectionEnd);
+
+      // Move cursor after tab
+      event.target.setSelectionRange(
+        selectionStart + 1,
+        selectionStart + 1
+      )
+    }
+  }
+
+  async function handleCopy() {
+    try {
+      await navigator.clipboard.writeText(value);
+      setCopied(true);
+
+      // Reset the "copied" message after 2 seconds
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (error) {
+      console.error('Failed to copy text:', error);
+    }
+  }
 
   return (
     <div className="container p-2 mx-auto">
       <div style={{ display: 'flex', justifyContent: 'space-around', gap: '1rem' }}>
-        <textarea style={{ width: '640px', height: '320px' }} value={value} onChange={(event) => {
+        <textarea style={{ width: '480px', height: '320px' }} value={value} onChange={(event) => {
           setValue(event.target.value);
-        }}></textarea>
-        <div className="tree">
+        }} onKeyDown={handleTabKey}></textarea>
+        <div className="tree" style={{ width: '480px', whiteSpace: 'pre-wrap' }}>
           {value}
         </div>
+      </div>
+      <div className="control">
+        <button onClick={handleCopy}>{ copied ? 'Copied' : 'Copy to clipboard' }</button>
       </div>
     </div>
   )
